0530a88d019581939edaf05bdb24934d09e8fb5c
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-07-17 11:23:38 +1000

    Update App.jsx

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from "./Home.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,4 +14,3 @@ function App() {
 }
 
 export default App
-
